b594d1fed7e0f30b0ba4b4be6ab94c78f854661f
David Coley <sdc285@msstate.edu>
2018-03-07 15:19:36 -0600

    Got a background working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 //import { Button } from 'react-native';
-//import swapi from "sw-api-js";
-
-//const swapiNode = require("swapi-node");
 
 class App extends Component {
   render() {
-    //const moon = swapiNode.getPerson(1).then(moon => console.log(moon));
     fetch("https://swapi.co/api/people/1")
       .then(resp => resp.json())
       .then(data => console.log(data.name));
@@ -19,9 +15,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
