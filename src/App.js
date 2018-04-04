6fe6bd2590032c206aa95c890a6768cb247b7067
David Coley <sdc285@msstate.edu>
2018-04-04 14:51:26 -0500

    text box on film calls api and removed react header

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         <Router>
           <div>
             <Route exact path="/films" component={Films} />
--- src/Films.js
+++ src/Films.js
@@ -9,81 +9,97 @@ import demo from "./pics/demo.jpg";
   return <img src={"../src/pics/demo.jpg"} width="640" height="360" />;
 } */
 
-const Films = () => (
-  <div>
-    <div className="leftnav">
-      <SideNav
-        highlightColor="#E91E63"
-        highlightBgColor="#00bcd4"
-        //defaultSelected="sales"
-      >
-        <Nav id="Home">
-          <NavText>
-            <Link to="/"> Home </Link>
-          </NavText>
-        </Nav>
+class Films extends Component {
+  componentDidMount() {
+    fetch("https://swapi.co/api/films/1")
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ films: data });
+        console.log(data);
+      });
+  }
 
-        <Nav id="Episode I">
-          <NavText> Episode I: The Phantom Menance </NavText>
-        </Nav>
+  render() {
+    return (
+      <div>
+        <div className="leftnav">
+          <SideNav
+            highlightColor="#E91E63"
+            highlightBgColor="#00bcd4"
+            //defaultSelected="sales"
+          >
+            <Nav id="Home">
+              <NavText>
+                <Link to="/"> Home </Link>
+              </NavText>
+            </Nav>
 
-        <Nav id="Episode II">
-          <NavText> Episode II: Attack of the Clones </NavText>
-        </Nav>
+            <Nav id="Episode I">
+              <NavText> Episode I: The Phantom Menance </NavText>
+            </Nav>
 
-        <Nav id="Episode III">
-          <NavText> Episode III: Revenge of the Sith </NavText>
-        </Nav>
+            <Nav id="Episode II">
+              <NavText> Episode II: Attack of the Clones </NavText>
+            </Nav>
 
-        <Nav id="Episode IV">
-          <NavText> Episode IV: A New Hope </NavText>
-        </Nav>
+            <Nav id="Episode III">
+              <NavText> Episode III: Revenge of the Sith </NavText>
+            </Nav>
 
-        <Nav id="Episode V">
-          <NavText> Episode V: The Empire Strikes Back </NavText>
-        </Nav>
+            <Nav id="Episode IV">
+              <NavText> Episode IV: A New Hope </NavText>
+            </Nav>
 
-        <Nav id="Episode VI">
-          <NavText> Episode VI: Return of the Jedi </NavText>
-        </Nav>
+            <Nav id="Episode V">
+              <NavText> Episode V: The Empire Strikes Back </NavText>
+            </Nav>
 
-        <Nav id="Episode VII">
-          <NavText> Episode VII: The Force Awakens </NavText>
-        </Nav>
-      </SideNav>
-    </div>
+            <Nav id="Episode VI">
+              <NavText> Episode VI: Return of the Jedi </NavText>
+            </Nav>
 
-    <div className="rightnav">
-      <SideNav
-        highlightColor="#E91E63"
-        highlightBgColor="#00bcd4"
-        defaultSelected="sales"
-      >
-        <Nav id="Characters">
-          <NavText> Characters </NavText>
-        </Nav>
+            <Nav id="Episode VII">
+              <NavText> Episode VII: The Force Awakens </NavText>
+            </Nav>
+          </SideNav>
+        </div>
 
-        <Nav id="Spacships">
-          <NavText> Spaceships </NavText>
-        </Nav>
+        <div className="rightnav">
+          <SideNav
+            highlightColor="#E91E63"
+            highlightBgColor="#00bcd4"
+            defaultSelected="sales"
+          >
+            <Nav id="Characters">
+              <NavText> Characters </NavText>
+            </Nav>
 
-        <Nav id="Vehicles">
-          <NavText> Vehicles </NavText>
-        </Nav>
+            <Nav id="Spacships">
+              <NavText> Spaceships </NavText>
+            </Nav>
 
-        <Nav id="Planets">
-          <NavText> Planets </NavText>
-        </Nav>
+            <Nav id="Vehicles">
+              <NavText> Vehicles </NavText>
+            </Nav>
 
-        <Nav id="Species">
-          <NavText> Species </NavText>
-        </Nav>
-      </SideNav>
-    </div>
-    <div>
-      <img src={demo} width="640" height="360" />
-    </div>
-  </div>
-);
+            <Nav id="Planets">
+              <NavText> Planets </NavText>
+            </Nav>
+
+            <Nav id="Species">
+              <NavText> Species </NavText>
+            </Nav>
+          </SideNav>
+        </div>
+        <div>
+          <img src={demo} width="640" height="360" />
+        </div>
+        <div className="opencrawl">
+          <text>{this.state && this.state.films.opening_crawl}</text>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Films;
