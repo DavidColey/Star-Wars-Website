df20d554e4a7c7baa45fa7db8114b0627a57a066
David Coley <sdc285@msstate.edu>
2018-05-30 16:20:44 -0500

    Added header and logo. Also changed background.

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ep1logo from "./pics/Phantom menance.png";
 
 const Home = () => (
   <div className="flex-container">
     <div className="flex-item">
       <Link to="/ep1">
-        <button className="buttonformat" title="Learn More" color="#841584">
-          Episode I: The Phantom Menance
-        </button>
+        <button className="buttonformat">Episode I: The Phantom Menance</button>
       </Link>
     </div>
 
     <div className="flex-item">
       <Link to="/ep2">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-
-          //accessibilityLabel="Learn more about this purple button"
-        >
+        <button className="buttonformat">
           Episode II: Attack of the Clones
         </button>
       </Link>
@@ -27,12 +20,7 @@ const Home = () => (
 
     <div className="flex-item">
       <Link to="/ep3">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-          //accessibilityLabel="Learn more about this purple button"
-        >
+        <button className="buttonformat">
           Episode III: Revenge of the Sith
         </button>
       </Link>
@@ -40,25 +28,13 @@ const Home = () => (
 
     <div className="flex-item">
       <Link to="/ep4">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-          //accessibilityLabel="Learn more about this purple button"
-        >
-          Episode IV: A New Hope
-        </button>
+        <button className="buttonformat">Episode IV: A New Hope</button>
       </Link>
     </div>
 
     <div className="flex-item">
       <Link to="/ep5">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-          //accessibilityLabel="Learn more about this purple button"
-        >
+        <button className="buttonformat">
           Episode V: The Empire Strikes Back
         </button>
       </Link>
@@ -66,27 +42,13 @@ const Home = () => (
 
     <div className="flex-item">
       <Link to="/ep6">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-          //accessibilityLabel="Learn more about this purple button"
-        >
-          Episode VI: Return of the Jedi
-        </button>
+        <button className="buttonformat">Episode VI: Return of the Jedi</button>
       </Link>
     </div>
 
     <div className="flex-item">
       <Link to="/ep7">
-        <button
-          className="buttonformat"
-          title="Learn More"
-          color="#841584"
-          //accessibilityLabel="Learn more about this purple button"
-        >
-          Episode VII: The Force Awakens
-        </button>
+        <button className="buttonformat">Episode VII: The Force Awakens</button>
       </Link>
     </div>
   </div>
